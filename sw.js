41d6a87b73cf074baee1815eb1f7d1de901a6440
hamidbaki2012-collab <hamidbaki2012@gmail.com>
2026-03-23 15:54:36 +0100

    Add radio tests

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'radio-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/background.jpg',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
